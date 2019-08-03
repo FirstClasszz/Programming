58ac96de04d763c9be01c264f26e816dfac19717
FirstClasszz <52350327+FirstClasszz@users.noreply.github.com>
2019-08-03 19:26:27 +0700

    Add files via upload

---
+++ Exercise4_Suphakorn_B.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+f="Foundation English:"
+FE=80
+g="General Business:"
+GB=70
+i="Introduction to Computer systems:"
+ICS=75
+C="Computer Programming:"
+CR=60.5
+print("-----your Score-----")
+print(f,FE)
+print(g,GB)
+print(i,ICS)
+print(C,CR)
+
+
+# In[ ]:
+
+
+
+
