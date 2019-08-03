f2242740643e0c312eaacfbfb9b66a081c191262
FirstClasszz <52350327+FirstClasszz@users.noreply.github.com>
2019-08-03 19:26:52 +0700

    Add files via upload

---
+++ Exercise5_1_Suphakorn_B.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+c=float(input("Enter number5"))
+g=float(input("Enter number6"))
+print(c,"+",g,"=",c+g)
+print(c,"-",g,"=",c-g)
+print(c,"*",g,"=",c*g)
+print(c,"/",g,"=",c/g)
+
+
+# In[ ]:
+
+
+
+
